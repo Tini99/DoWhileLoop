e0c402f05f9d4e8d543b1c808696e45a1908da48
Tini Gisa <1814759@nzse.ac.nz>
2021-06-14 21:01:05 +1200

    Add project files.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            i = 6;
+
+            do
+            {
+                i = i + 1;
+            }
+            while (i < 5);
+        }
+    }
+}
